7fa22b9b98d8d2509e52a5e5d794919ae23ca986
shiyuexu <shiyuexu@dhcp-10-103-249-243.wireless.northwestern.private>
2025-10-03 11:54:28 -0500

    Fetch Data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Banner from './components/Banner'
 import CourseCardList from './components/CoursCardList'
+import { useJsonQuery } from "./utilities/fetch";
+
+
+const DATA_URL =
+  "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php";
 
 export type Course = {
   term: string,
@@ -10,40 +15,50 @@ export type Course = {
   meets: string,
   title: string
 }
-const schedule = {
-  title: "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-  };
+
+// const schedule = {
+//   title: "CS Courses for 2018-2019",
+//   "courses": {
+//     "F101" : {
+//       "term": "Fall",
+//       "number": "101",
+//       "meets" : "MWF 11:00-11:50",
+//       "title" : "Computer Science: Concepts, Philosophy, and Connections"
+//     },
+//     "F110" : {
+//       "term": "Fall",
+//       "number": "110",
+//       "meets" : "MWF 10:00-10:50",
+//       "title" : "Intro Programming for non-majors"
+//     },
+//     "S313" : {
+//       "term": "Spring",
+//       "number": "313",
+//       "meets" : "TuTh 15:30-16:50",
+//       "title" : "Tangible Interaction Design and Learning"
+//     },
+//     "S314" : {
+//       "term": "Spring",
+//       "number": "314",
+//       "meets" : "TuTh 9:30-10:50",
+//       "title" : "Tech & Human Interaction"
+//     }
+//   }
+//   };
+
+interface Schedule {
+  title: string;
+  courses: {[id: string]: Course};
+}
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [json, isLoading, error] = useJsonQuery(DATA_URL);
 
-  
+  if (error) return <h1>Error loading course data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading course data...</h1>;
+  if (!json) return <h1>No course data found</h1>;
+
+  const schedule = json as Schedule;
 
   return (
     <div className="text-center">
@@ -52,34 +67,7 @@ const App = () => {
           <Banner title={schedule.title} />
           <CourseCardList courses={schedule.courses} />
         </div>
-        <img src={reactLogo} className="h-[20vmin] pointer-events-none motion-safe:animate-logo-spin" alt="logo" />
-        <img src={viteLogo} className="h-[20vmin] pointer-events-none motion-safe:animate-logo-spin" alt="logo" />
-        <p className="m-4">Hello React + Vite!</p>
-        <p>
-          <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded " onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p className="italic text-gray-400">
-          Edit <code>App.tsx</code> to test hot module replacement (HMR).
-        </p>
-        <p className='mt-4'>
-          <a className="bg-cyan-300 hover:bg-cyan-400 text-black font-bold py-2 px-4 rounded"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a className='bg-cyan-300 hover:bg-cyan-400 text-black font-bold py-2 px-4 rounded'
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        
       </header>
     </div>
   )
